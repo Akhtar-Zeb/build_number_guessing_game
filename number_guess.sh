ca40ce5dcd3c22cc5b2f803d09a98cb43daf5e39
Akhtar Zeb <akhtarzebtmg@gmail.com>
2025-10-22 08:47:16 -0400

    feat: get stats or insert the new user if any

--- number_guess.sh
+++ number_guess.sh
@@ -8,7 +8,16 @@ read USERNAME
 
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
+# check if user is in db
 if [[ -z $USER_ID ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
+  # insert the user
+  USER_ID=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  
+# else user in db
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
